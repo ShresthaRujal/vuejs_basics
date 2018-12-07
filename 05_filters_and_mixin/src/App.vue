4ca089a0154f240c0c5b2f01c0130d7e0e7cbc8d
Ruzal Shrestha <shrestharujal1928@gmail.com>
2018-12-07 21:10:37 +0545

    filters and mixin

---
+++ 05_filters_and_mixin/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="app">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Filters and Mixin</h1>
+                <p>{{text | toUpperCase}}</p>
+                <p>{{text | toLowerCase}}</p>
+
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">
+                        {{fruit}}
+                    </li>
+                </ul>
+                <app-list></app-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import List from './List.vue';
+import {fruitMixin} from './fruitMixin';
+export default {
+    mixins:[fruitMixin],
+    data(){
+        return{
+            text: 'Hello World!', 
+            // fruits:['Apple','Banana','Mango','Papaya'],
+            // filterText:''
+        }
+    },
+    filters:{
+        toUpperCase(value){
+            return value.toUpperCase();
+        } 
+    },
+    // computed:{
+    //     filteredFruits(){
+    //         return this.fruits.filter((element) => {
+    //             return element.match(this.filterText);
+    //         });
+    //     }
+    // },
+    components:{
+        appList: List
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 05_filters_and_mixin/src/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="app">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Filters and Mixin</h1>
+
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">
+                        {{fruit}}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {fruitMixin} from './fruitMixin';
+export default {
+    mixins: [fruitMixin]
+}
+</script>
+
+<style scoped>
+
+</style>
